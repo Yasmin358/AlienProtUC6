38b2f5acdf72d6defd2d5bef1530e17232a9297e
Yasmin358 <amorin.yasmin378@gmail.com>
2021-04-06 16:25:17 -0300

    Atividade 1 - Versão 1.0

---
+++ Projeto3d/Assets/Scripts/ArmaController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmaController : MonoBehaviour
+{
+    public GameObject projetil;
+    public GameObject posição;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            Instantiate(projetil, new Vector3(posição.transform.position.x, posição.transform.position.y, posição.transform.position.z), Quaternion.identity);
+        }
+    }
+
+}
---
+++ Projeto3d/Assets/Scripts/ParedeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParedeController : MonoBehaviour
+{
+    public int paredeVida = 3;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paredeVida == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider outro)
+    {
+        if (outro.gameObject.CompareTag("projetil"))
+        {
+            paredeVida = paredeVida - 1;
+        }
+    }
+}
